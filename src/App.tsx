43f743e505529501d7380e4bd00ed2f6249bb56b
Sowmya <sowmya.dhandapani@flexibees.com>
2025-05-17 13:33:28 +0530

    Changes for Table Creation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
+const renewals = client.models.JobRenewals.create({
+  notes: 'New todo content', job_id: 1, date_of_renewal: new Date(), 
+  renewal_period: 1, new_client_pricing: 0, new_candidate_pricing: 0, new_hours: 0, status: 'active' })
 
 function App() {
   const [jobRenewals, setTodos] = useState<Array<Schema["JobRenewals"]["type"]>>([]);
@@ -14,7 +17,8 @@ function App() {
   }, []);
 
   function createTodo() {
-    client.models.JobRenewals.create({ notes: window.prompt("Todo content") });
+    console.log("Create Renewal")
+    client.models.JobRenewals.create(renewals);
   }
 
   function updateTodo(id: integer) {
@@ -24,21 +28,35 @@ function App() {
   return (
     <main>
       <h1>Job Renewals</h1>
-      <button onClick={createTodo}> + new</button>
+      <button onClick={createTodo ()}> + new</button>
       <table>
-        {jobRenewals.map((renewal) => (
-          <tr key={renewal.id}>
-            <td>{renewal.job_id}</td>
-            <td>{renewal.date_of_renewal}</td>
-            <td>{renewal.renewal_period}</td>
-            <td>{renewal.new_candidate_pricing}</td>
-            <td>{renewal.new_hours}</td>
-            
-          <td>
-            <button onClick={() => updateTodo(renewal.id)}>Update first</button>
-          </td>
+        <thead>
+          <tr>
+            <th>Job ID</th>
+            <th>Date of Renewal</th>
+            <th>Renewal Period</th>
+            <th>New Client Pricing</th>
+            <th>New Candidate Pricing</th>
+            <th>New Hours</th>
+          </tr>
+        </thead>
+        <tbody>
+          {jobRenewals.map((renewal) => (
+
+            <tr key={renewal.id}>
+              <td>{renewal.job_id}</td>
+              <td>{renewal.date_of_renewal}</td>
+              <td>{renewal.renewal_period}</td>
+              <td>{renewal.new_candidate_pricing}</td>
+              <td>{renewal.new_hours}</td>
+
+              <td>
+                <button onClick={() => updateTodo(renewal.id)}>Update first</button>
+              </td>
             </tr>
-        ))}
+
+          ))}
+        </tbody>
       </table>
     </main>
   );
